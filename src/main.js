b5f97c04edb725ac0d663efbae087a98f4d61a7a
Chris Annunziato <cmannunziato@gmail.com>
2025-02-22 13:59:09 -0500

    added initial structure

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const app = document.querySelector('#app')
+
+const headerSection = document.createElement('header')
+headerSection.id = 'header'
+const footerSection = document.createElement('footer')
+footerSection.id = 'footer'
+const mainSection = document.createElement('section')
+mainSection.id = 'main'
+
+
+app.appendChild(headerSection)
+app.appendChild(mainSection)
+app.appendChild(footerSection)
+
+
+
