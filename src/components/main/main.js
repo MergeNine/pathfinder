717ed049dba8ea75228fff3e9642621c627ee1d7
Chris Annunziato <cmannunziato@gmail.com>
2025-02-22 15:37:37 -0500

    2d array of buttons

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,5 +1,14 @@
 
 
 const main = document.querySelector('#main')
-main.innerHTML = "Hello World!!"
 
+for(let i = 0; i < 28; i++){
+    const row  = document.createElement('button')
+    row.className = 'row'
+    main.appendChild(row)
+    for(let j = 0; j <20; j++) {
+        const col  = document.createElement('button')
+        col.className = 'col'
+        row.appendChild(col)
+    }
+}
